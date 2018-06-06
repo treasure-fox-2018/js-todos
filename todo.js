71a8a802ade47e48eaee43aceee2357d0e2f21fa
adrowicaksono <adrowicaksono@gmail.com>
2018-06-06 23:25:41 +0700

    to do apps

--- todo.js
+++ todo.js
@@ -1,1 +1,34 @@
+const Controller = require('./controller.js')
+
+
+
+var commandArgv = process.argv;
+var command = commandArgv[2]
+var value = commandArgv[3]
+
+var arrTag = [];
+
+for(let a = 0; a< commandArgv.length; a++){
+    if(a > 3){
+        arrTag.push(commandArgv[a])
+    }
+}
+
+switch(command){
+    case 'help' : {Controller.help(); break}
+    case 'list' : {Controller.listTask(); break}
+    case 'add'  : {Controller.addTask(value); break}
+    case 'findById' : {Controller.findId(value);break}
+    case 'delete' : {Controller.delete(value);break}
+    case 'completed' : {Controller.completed(value, true);break}
+    case 'uncompleted' : {Controller.completed(value, false);break}
+    case 'list:created' : {Controller.sortByCreated(value);break}
+    case 'list:completed' : {Controller.sortByCompleted(value);break}
+    case 'list:tag' : {Controller.addTag(value, arrTag);break}
+    case 'filter' : {Controller.findTag(value);break}
+    
+}
+
+
+ 
 
