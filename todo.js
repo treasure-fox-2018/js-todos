5573615fe6b2e0f9b8ec7bd96d78c3b7099b23ed
Yusuf Salman Noerinaldy Siregar <yusufsiregar5@gmail.com>
2018-06-07 22:20:45 +0700

    update file

--- todo.js
+++ todo.js
@@ -1,6 +1,4 @@
-"use strict"
-
-const serverSide = require("./controller1.js");
+const controller = require("./controller.js");
 
 let input = process.argv;
 
@@ -8,6 +6,31 @@ let command = input[2];
 
 let datum = input[3];
 
-if (command === undefined) {
-  serverSide.displayHelp()
+let arr = input.slice(4);
+
+if (!command || command === "help") {
+  controller.help();
+} else if (command === "list") {
+  controller.list();
+} else if (command === "add") {
+  controller.add(datum);
+} else if (command === "findById") {
+  controller.find(datum);
+} else if (command === "delete") {
+  controller.delete(datum);
+} else if (command === "complete") {
+  controller.check(datum);
+} else if (command === "uncomplete") {
+  controller.uncheck(datum);
+} else if (command === "list:created") {
+  controller.listOutstanding(datum);
+} else if (command === "list:completed") {
+  controller.listCompleted(datum);
+} else if (command === "tag") {
+  controller.assignTag(datum, arr);
+} else if (command.split(":")[0] === "filter") {
+  let tag = command.split(":")[1];
+  controller.getTasksByTags(tag);
 }
+
+module.exports = datum;
