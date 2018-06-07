63265d2b7f3f0d56c880afb5439b92e20c369edc
Wahyudi Setiaji <wahyudisetiaji@gmail.com>
2018-06-07 14:58:02 +0700

    Add todo

--- todo.js
+++ todo.js
@@ -1,1 +1,35 @@
+const fs = require('fs');
+var list = fs.readFileSync('./data.json', 'utf8')
 
+class Todo {
+  constructor() {
+
+  }
+
+  help() {
+    console.log();
+    console.log('   $ node todo.js                              # Will call help');
+    console.log('   $ node todo.js help                         # Menampilkan command apa saja yang tersedia');
+    console.log('   $ node todo.js list                         # Melihat daftar TODO');
+    console.log('   $ node todo.js add <task_content>           # Menambahkan TODO ke dalam list');
+    console.log('   $ node todo.js findById <task_id>           # Melihat detail TODO sesuai `task_id` nya');
+    console.log('   $ node todo.js delete <task_id>             # Menghapus TODO sesua `task_id` nya');
+    console.log('   $ node todo.js complete <task_id>           # Manandai status TODO selesai');
+    console.log('   $ node todo.js uncomplete <task_id>         # Menandai status TODO belum selesai');
+    console.log();
+  }
+}
+
+class List {
+  constructor() {
+  }
+}
+
+let todo = new Todo()
+
+
+var argv = process.argv
+
+if (argv[2] === 'help') {
+  todo.help()
+}
