9e95069384fcc2539a3b5ccc77c39bddef27858e
Ade Fahri Syany <36616621+fahrisyany@users.noreply.github.com>
2018-06-08 12:55:11 +0700

    fix bugs

--- view.js
+++ view.js
@@ -1,22 +1,12 @@
 
 class View {
 
-    static help_v() {
-
-        console.log('node todo.js help')
-        console.log('node todo.js list')
-        console.log('node todo.js add <task_content>')
-        console.log('node todo.js findById <task_id>')
-        console.log('node todo.js delete <task_id>')
-        console.log('node todo.js complete <task_id>')
-        console.log('node todo.js uncomplete <task_id>')
-    }
 
     static list_v(variable) {
         console.log('hasilnya (ada nomor ID dan list todonya): ')
         let listofData = JSON.parse(variable)
         for (let obj of listofData) {
-            if (obj.status == 'complete') {
+            if (obj.status =="complete") {
                 console.log(`${obj.id}. [X] ${obj.task}`)
             }
             else {
@@ -35,7 +25,7 @@ class View {
     static SortedDate_v(variable) {
         console.log('hasilnya (ada nomor ID dan list todonya): ')
         for (let obj of variable) {
-            if (obj.status == 'complete') {
+            if (obj.status =="complete") {
                 console.log(`${obj.id}. [X] ${obj.task}`)
             }
             else {
@@ -47,13 +37,25 @@ class View {
     static ShowFilteredData_v(taggedList) {
         console.log('List yang ter-Filter: ')
         for (let obj of taggedList) {
-            if (obj.status == 'complete') {
+            if (obj.status == "complete") {
                 console.log(`${obj.id}. [X] ${obj.task} [${obj.tag}]`)
             }
             else {
                 console.log(`${obj.id}. [ ] ${obj.task} [${obj.tag}]`)
             }
         }
+    }
+
+
+    static help_v() {
+
+        console.log('node todo.js help')
+        console.log('node todo.js list')
+        console.log('node todo.js add <task_content>')
+        console.log('node todo.js findById <task_id>')
+        console.log('node todo.js delete <task_id>')
+        console.log('node todo.js complete <task_id>')
+        console.log('node todo.js uncomplete <task_id>')
     }
 }
 
