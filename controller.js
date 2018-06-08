f669d861652f92afbf3fd4a7b08832d83331c150
Your Name <fahrisyany@gmail.com>
2018-06-08 08:35:50 +0700

    done

--- controller.js
+++ controller.js
@@ -1,1 +1,97 @@
-var fs = require('fs');
+let Model = require('./model')
+let View = require('./view')
+
+
+
+class Controller {
+
+    
+    static getHelp_c() {
+        View.help_v()
+    }
+
+    static getTodoList_c() {
+        let result = Model.toDoList_m()
+        View.list_v(result)
+    }
+
+    static addTodoList_c(variable) {
+        Model.setTodoList_m(variable)
+
+    }
+
+    static findTask_c(variable) {
+        let result = Model.getTask_m(variable)
+        View.showFindTask_v(result)
+    }
+
+    static deleteTask_c(variable) {
+        Model.deletedTask_m(variable)
+
+    }
+
+    static completedTask_c(variable) {
+
+        Model.completedTask_m(variable)
+    }
+
+    static incompletedTask_c(variable) {
+
+        Model.incompletedTask_m(variable)
+    }
+
+    static sortTask_c(command) {
+        let sorted = Model.SortcreatedDate_m(command)
+        View.SortedDate_v(sort)
+
+    }
+
+    static sortCompletedTask_c(command) {
+        let sorted = Model.SortcompleteDate_m(command)
+        View.SortedDate_v(sort)
+
+    }
+
+    static putTag_c(id, tags) {
+
+
+        Model.Tag_m(id, tags)
+    }
+
+    static filter_c(variable) {
+        let taggedList = Model.showFilter_m(variable)
+        View.ShowFilteredData_v(taggedList)
+
+    }
+
+
+}
+
+module.exports = Controller
+
+
+
+
+
+module.exports = Controller
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = Controller
--- todo.js
+++ todo.js
@@ -1,39 +1,48 @@
+let Controller = require('./controller')
 
-var fs = require('fs');
-let Controller = require("./controller")
+let argv = process.argv.slice(2)
+let command = argv[0]
 
-
-
-'-----------todo.js--------'
-
-
-if (command === 'add') {
-
-    Controller.add(taskname)
-
+if(command === 'add'){
+    let variable = argv.slice(1)
+    var Datainfo = variable.join(' ')    
+}
+else{
+    var variable = argv[1]
+    let warp = argv.slice(2)
+    var tag = warp.join(' ')
 }
 
-
-'-----------controller.js--------'
-
-class controller {
-
-    static Add(taskName) {
-
-        Model.add(taskName)
-        let.todoList
-    }
-
+if(command === 'help' || command === undefined){
+    Controller.getHelp_c();
 }
-
-
-'-----------model.js-----===---'
-
-class Model {
-
-    static list() {
-        // readfile
-        // return readfile
-
-    }
+else if(command === 'list'){
+    Controller.getTodoList_c();
+}
+else if(command === 'add'){
+    Controller.addTodoList_c(Datainfo);
+}
+else if(command === 'findById'){
+    Controller.findTask_c(variable)
+}
+else if(command === 'delete'){
+    Controller.deleteTask_c(variable)
+}
+else if(command === 'complete'){
+    Controller.completedTask_c(variable)
+}
+else if(command === 'uncomplete'){
+    Controller.incompletedTask_c(variable)
+}
+else if(command === 'list:created'){
+    Controller.sortTask_c(variable)
+}
+else if(command === 'list:complete'){
+    Controller.sortCompletedTask_c(variable)
+}
+else if(command === 'tag'){
+    Controller.putTag_c(variable,tag)
+}
+else if(command === 'filter:'){
+    Controller.filter_c(variable)
 }
